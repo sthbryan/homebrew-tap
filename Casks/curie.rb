99ac4834e260931d3e6daf4be854a8d8337bab59
Bryan Villafuerte <bryamu.u@gmail.com>
2026-08-17 00:29:44 -0600

    feat: bump curie cask to v0.8.0 with bundle id migration

--- Casks/curie.rb
+++ Casks/curie.rb
@@ -1,6 +1,6 @@
 cask "curie" do
-  version "0.7.0"
-  sha256 "87e3a4df1c3c6f29bfa7402233511ed2a3e80f424083013b45c19522d6a87542"
+  version "0.8.0"
+  sha256 "f66b2de0a98ccd964bdcc07dd1f5dc767b27adbfb0e8860e9152ac153561028f"
 
   url "https://github.com/sthbryan/curie/releases/download/v#{version}/Curie_#{version}_aarch64.dmg"
   name "Curie"
@@ -23,13 +23,35 @@ cask "curie" do
                    args: ["--force", "--deep", "--sign", "-", app_path]
     system_command "/usr/bin/xattr",
                    args: ["-cr", app_path]
+
+    # Migrate user data from the old bundle id so an upgrade from v0.7.x
+    # (where the id was com.curie.app) keeps settings/state.
+    from = "com.curie.app"
+    to   = "com.justcallmebryan.curie"
+    home = ENV.fetch("HOME")
+    [
+      ["Application Support", false],
+      ["Caches",              false],
+      ["Preferences",         true],
+      ["WebKit",              false],
+    ].each do |sub, plist|
+      src = File.join(home, "Library", sub, plist ? "#{from}.plist" : from)
+      next unless File.exist?(src)
+      dst = src.sub(from, to)
+      if File.exist?(dst)
+        puts "  [skip] #{sub}: destination already exists"
+      else
+        puts "  [move] #{sub}: #{File.basename(src)} -> #{File.basename(dst)}"
+        File.rename(src, dst)
+      end
+    end
   end
 
   zap trash: [
-    "~/Library/Application Support/com.curie.app",
-    "~/Library/Caches/com.curie.app",
-    "~/Library/Preferences/com.curie.app.plist",
-    "~/Library/WebKit/com.curie.app",
+    "~/Library/Application Support/com.justcallmebryan.curie",
+    "~/Library/Caches/com.justcallmebryan.curie",
+    "~/Library/Preferences/com.justcallmebryan.curie.plist",
+    "~/Library/WebKit/com.justcallmebryan.curie",
   ]
 
   caveats <<~EOS
